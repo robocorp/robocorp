a6ff49060c8322e1686bc52c5cb311344a7aea9a
Fabio Zadrozny <fabiofz@gmail.com>
2023-03-20 11:05:57 -0300

    Fixed issue where register callbacks could be called twice.

--- tests/robocorp_logging_tests/test_log_api.py
+++ tests/robocorp_logging_tests/test_log_api.py
@@ -44,30 +44,33 @@ def test_log_api_without_with_statments(tmpdir) -> None:
     from robocorp_logging import iter_decoded_log_format_from_log_html
 
     log_target = Path(tmpdir.join("log.html"))
-    robocorp_logging.setup_auto_logging()
+    ctx = robocorp_logging.setup_auto_logging()
     try:
-        check = reload(check)
+        try:
+            check = reload(check)
 
-        robocorp_logging.add_log_output(
-            tmpdir, max_file_size="30kb", max_files=1, log_html=log_target
-        )
+            robocorp_logging.add_log_output(
+                tmpdir, max_file_size="30kb", max_files=1, log_html=log_target
+            )
 
-        robocorp_logging.log_start_suite("Root Suite", "root", str(tmpdir))
-        robocorp_logging.log_start_task("my_task", "task_id", 0, [])
+            robocorp_logging.log_start_suite("Root Suite", "root", str(tmpdir))
+            robocorp_logging.log_start_task("my_task", "task_id", 0, [])
 
-        check.some_method()
+            check.some_method()
 
-        robocorp_logging.log_end_task("my_task", "task_id", "PASS", "Ok")
-        robocorp_logging.log_end_suite("Root Suite", "root", str(tmpdir))
+            robocorp_logging.log_end_task("my_task", "task_id", "PASS", "Ok")
+            robocorp_logging.log_end_suite("Root Suite", "root", str(tmpdir))
 
-        assert not log_target.exists()
+            assert not log_target.exists()
+        finally:
+            robocorp_logging.close_log_outputs()
+
+        assert log_target.exists()
+        start_method_messages = []
+        for v in iter_decoded_log_format_from_log_html(log_target):
+            if v["message_type"] == "SK":
+                start_method_messages.append(v)
+
+        assert len(start_method_messages) > 1
     finally:
-        robocorp_logging.close_log_outputs()
-
-    assert log_target.exists()
-    start_method_messages = []
-    for v in iter_decoded_log_format_from_log_html(log_target):
-        if v["message_type"] == "SK":
-            start_method_messages.append(v)
-
-    assert len(start_method_messages) > 1
+        ctx.__exit__(None, None, None)
