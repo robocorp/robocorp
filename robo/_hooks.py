fe6ad213f3144efa8240431ade506b3b7c8aae65
Fabio Zadrozny <fabiofz@gmail.com>
2023-03-23 12:28:36 -0300

    Update the logging dependency version.

--- robo/_hooks.py
+++ robo/_hooks.py
@@ -10,7 +10,6 @@ from robo._protocols import (
 # Called as on_task_func_found(task: ITask)
 on_task_func_found: IOnTaskFuncFoundCallback = Callback()
 
-
 # Called as before_collect_tasks(path: Path, task_name: str)
 before_collect_tasks: IBeforeCollectTasksCallback = Callback()
 
