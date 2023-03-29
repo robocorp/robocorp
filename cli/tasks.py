f898d2ea88f66010a6aeed19df981b5e4bb08277
Ossi Rajuvaara <ossi@robocorp.com>
2023-03-29 17:11:40 +0300

    Fix unittests

--- cli/tasks.py
+++ cli/tasks.py
@@ -8,12 +8,26 @@ def poetry(ctx, *parts):
 
 @task
 def lint(ctx):
+    """Run static analysis and formatting checks"""
     poetry(ctx, "ruff robo_cli")
     poetry(ctx, "black --check robo_cli")
     poetry(ctx, "isort --check robo_cli")
 
 
 @task
+def test(ctx):
+    """Run unittests"""
+    poetry(ctx, "pytest")
+
+
+@task
 def pretty(ctx):
+    """Automatically format code"""
     poetry(ctx, "black robo_cli tests")
     poetry(ctx, "isort robo_cli tests")
+
+
+@task
+def build(ctx):
+    """Build executable"""
+    poetry(ctx, "pyinstaller robo_cli.spec")
--- cli/tests/test_templates.py
+++ cli/tests/test_templates.py
@@ -3,26 +3,26 @@ from pathlib import Path
 
 import pytest
 
-from robo_cli import rcc
+from robo_cli import templates
 
 
 @pytest.fixture
 def tempdir():
     with tempfile.TemporaryDirectory() as tempdir:
-        yield tempdir
+        yield Path(tempdir)
 
 
 def test_blank(tempdir):
-    tempdir_path = Path(tempdir)
-    path = rcc.new_project(tempdir_path / "blank", "blank")
+    path = tempdir / "blank"
+    templates.copy_template(path, "blank")
     assert (path / ".gitignore").exists()
     assert (path / "pyproject.toml").exists()
     assert (path / "tasks.py").exists()
 
 
 def test_browser(tempdir):
-    tempdir_path = Path(tempdir)
-    path = rcc.new_project(tempdir_path / "browser", "browser")
+    path = tempdir / "browser"
+    templates.copy_template(path, "browser")
     assert (path / ".gitignore").exists()
     assert (path / "pyproject.toml").exists()
     assert (path / "tasks.py").exists()
