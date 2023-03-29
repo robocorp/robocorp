71dc1d3b9fb98d145c0271927e5ac3fd96e687a5
Kerkko Pelttari <kerkko@robocorp.com>
2023-03-29 11:54:01 +0300

    Also test browser template

--- cli/tests/test_robo_new.py
+++ cli/tests/test_robo_new.py
@@ -12,9 +12,17 @@ def tempdir():
         yield tempdir
 
 
-def test_new(tempdir):
+def test_blank(tempdir):
     tempdir_path = Path(tempdir)
     path = rcc.new_project(tempdir_path / "blank", "blank")
+    assert (path / ".gitignore").exists()
+    assert (path / "pyproject.toml").exists()
+    assert (path / "tasks.py").exists()
+
+
+def test_browser(tempdir):
+    tempdir_path = Path(tempdir)
+    path = rcc.new_project(tempdir_path / "browser", "browser")
     assert (path / ".gitignore").exists()
     assert (path / "pyproject.toml").exists()
     assert (path / "tasks.py").exists()
