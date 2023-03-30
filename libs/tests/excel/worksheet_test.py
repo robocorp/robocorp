fb98f01e23398063b970fcdf79a2ceb19b23f119
Kerkko Pelttari <kerkko@robocorp.com>
2023-03-30 16:52:52 +0300

    excel tests

---
+++ libs/tests/excel/worksheet_test.py
@@ -0,0 +1,15 @@
+import pytest
+
+
+from robo.libs.excel.excel import create_workbook
+
+
+@pytest.fixture
+def workbook():
+    return create_workbook("xlsx")
+
+
+def test_out_of_index_worksheet(workbook):
+    # TODO: should there be a default 0th sheet, if you create an empty one
+    with pytest.raises(IndexError):
+        workbook.worksheet(1)
