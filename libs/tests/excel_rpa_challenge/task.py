1dfed0cc77f2c9c7396c3561d0cb6b717d10ef48
Kerkko Pelttari <kerkko@robocorp.com>
2023-03-31 14:25:02 +0300

    Add requests dep to libs, fix rpa challenge

--- libs/tests/excel_rpa_challenge/task.py
+++ libs/tests/excel_rpa_challenge/task.py
@@ -2,11 +2,13 @@ from pathlib import Path
 from time import sleep
 from playwright.sync_api import Page
 from robo.libs.browser import open_url
+from robo.libs.http import download
 from robo.libs.excel import open_workbook, Table
 
 
 def read_people_from_excel() -> Table:
     table = open_workbook("challenge.xlsx").worksheet("Sheet1").as_table(header=True)
+    print(table)
     return table
 
 
@@ -14,23 +16,24 @@ def fill_and_submit_form(page: Page, person):
     page.fill('//input[@ng-reflect-name="labelFirstName"]', person["First Name"])
     page.fill('//input[@ng-reflect-name="labelLastName"]', person["Last Name"])
     page.fill('//input[@ng-reflect-name="labelCompanyName"]', person["Company Name"])
-    #     Input Text    alias:Company Name    ${person}[Company Name]
     page.fill('//input[@ng-reflect-name="labelRole"]', person["Role in Company"])
-    #     Input Text    alias:Role in Company    ${person}[Role in Company]
     page.fill('//input[@ng-reflect-name="labelAddress"]', person["Address"])
-    #     Input Text    alias:Address    ${person}[Address]
     page.fill('//input[@ng-reflect-name="labelEmail"]', person["Email"])
     page.fill('//input[@ng-reflect-name="labelPhone"]', str(person["Phone Number"]))
-    page.click("text=Submit")
+    page.click("input:text('Submit')")
 
 
 def task_solve_challenge():
     page = open_url("http://rpachallenge.com/", headless=False)
-    #     Download    http://rpachallenge.com/assets/downloadFiles/challenge.xlsx    overwrite=${TRUE}
-    page.click("text=Start")
+    download("http://rpachallenge.com/assets/downloadFiles/challenge.xlsx")
+    page.click("button:text('Start')")
     people = read_people_from_excel()
+    i = 0
     for person in people:
+        i = i + 1
+        print(person)
         fill_and_submit_form(page, person)
+    print(f"amount of rows inputted {i}")
     # TODO: get the actual path of ARTIFACTS_DIR
     element = page.query_selector("css=div.congratulations")
     element.screenshot(path=Path("output") / "screenshot.png")
