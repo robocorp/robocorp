43f0ea514075d751770ae8688aa0ba5cbd3de08e
Antero Vaissi <antero@robocorp.com>
2023-04-03 13:47:57 +0300

    rename imported function to test, cleanup with black

--- libs/src/robo/libs/http/_http.py
+++ libs/src/robo/libs/http/_http.py
@@ -8,17 +8,15 @@ def download(
     target_file: Optional[str] = None,
     stream=True,
 ) -> dict:
-    filename = Path(target_file) if target_file else Path(url.split('/')[-1])
+    filename = Path(target_file) if target_file else Path(url.split("/")[-1])
     filename = str(filename)
     # NOTE the stream=True parameter below
     with requests.get(url, stream=stream) as r:
         r.raise_for_status()
-        with open(filename, 'wb') as f:
+        with open(filename, "wb") as f:
             for chunk in r.iter_content(chunk_size=8192):
                 # If you have chunk encoded response uncomment if
                 # and set chunk_size parameter to None.
-                #if chunk:
+                # if chunk:
                 f.write(chunk)
     return filename
-
-
--- libs/tests/browser/test_open_available_browser.py
+++ libs/tests/browser/test_open_available_browser.py
@@ -1,6 +1,6 @@
 import pytest
 
-from robo.libs.browser.browser import open_available_browser, _get_executable_path
+from robo.libs.browser.browser import open_browser, _get_executable_path
 
 
 def test_get_executable_path():
@@ -13,7 +13,7 @@ def test_get_executable_path():
 
 @pytest.mark.skip(reason="no way of currently testing this")
 def test_simple_page_actions():
-    browser = open_available_browser(headless=False)
+    browser = open_browser(headless=False)
     context = browser.new_context()
     page = context.new_page()
     page.goto("http://rpachallenge.com/")
