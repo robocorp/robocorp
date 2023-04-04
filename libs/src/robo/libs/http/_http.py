36e272ee728e0128df9cbda25d2a67120a53242c
Antero Vaissi <antero@robocorp.com>
2023-04-04 14:26:30 +0300

    Improve HTTP download to support override and dir as target

--- libs/src/robo/libs/http/_http.py
+++ libs/src/robo/libs/http/_http.py
@@ -1,22 +1,50 @@
 from pathlib import Path
 import requests
-from typing import Any, Optional, Union
+from urllib.parse import urlparse
+
+from typing import Any, Optional
+from robo.libs._types import PathType
+
+
+def _create_or_overwrite_target_file(
+    path: PathType,
+    response: Any,
+    overwrite: bool,
+) -> Path:
+    CHUNK_SIZE = 32768
+    path = Path(path)
+    path.parent.mkdir(parents=True, exist_ok=True)
+    file_exists = path.is_file()
+    if not file_exists or (file_exists and overwrite):
+        with open(path, "wb") as f:
+            for chunk in response.iter_content(CHUNK_SIZE):
+                if chunk:  # filter out keep-alive new chunks
+                    f.write(chunk)
+    return path
 
 
 def download(
     url: str,
-    target_file: Optional[str] = None,
+    target_file: Optional[PathType] = None,
+    overwrite: bool = False,
     stream=True,
-) -> dict:
-    filename = Path(target_file) if target_file else Path(url.split("/")[-1])
-    filename = str(filename)
-    # NOTE the stream=True parameter below
-    with requests.get(url, stream=stream) as r:
-        r.raise_for_status()
-        with open(filename, "wb") as f:
-            for chunk in r.iter_content(chunk_size=8192):
-                # If you have chunk encoded response uncomment if
-                # and set chunk_size parameter to None.
-                # if chunk:
-                f.write(chunk)
-    return filename
+) -> Path:
+    dirname = Path()
+    filename = None
+
+    if target_file is not None:
+        target = Path(target_file)
+        if target.is_dir():
+            dirname = target
+        else:
+            dirname = target.parent
+            filename = target.name
+
+    if filename is None:
+        filename = urlparse(url).path.rsplit("/", 1)[-1] or "downloaded.html"
+
+    with requests.get(url, stream=stream) as response:
+        response.raise_for_status()
+        path = _create_or_overwrite_target_file(dirname / filename, response, overwrite)
+
+    return path
