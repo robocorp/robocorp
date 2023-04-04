110c4e4fb74f8e7e460560a2f86087576194de25
Fabio Zadrozny <fabiofz@gmail.com>
2023-04-04 20:33:03 -0300

    Decode message related to restarting the traceback in a new file in javascript.

--- logging/output-webview/src/decoder.ts
+++ logging/output-webview/src/decoder.ts
@@ -135,6 +135,7 @@ const _MESSAGE_TYPE_INFO = {
     "TG": _decode("tag:oid"),
     "S": _decode("start_time_delta:float"),
     "STB": _decode("message:oid, time_delta_in_seconds:float"),
+    "RTB": _decode("message:oid, time_delta_in_seconds:float"),
     "TBE": _decode("source:oid, lineno:int, method:oid, line_content:oid"),
     "TBV": _decode("variable:oid"),
     "ETB": _decode("time_delta_in_seconds:float"),
