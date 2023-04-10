75f789a9e50b75b05c2da0f9cc1f6a6b09ac35ff
Fabio Zadrozny <fabiofz@gmail.com>
2023-04-10 15:47:39 -0300

    Set robocorp-logging version to 0.0.3

--- src/robocorp_logging/__init__.py
+++ src/robocorp_logging/__init__.py
@@ -25,7 +25,7 @@ from robocorp_logging.protocols import OptExcInfo, LogHTMLStyle
 if typing.TYPE_CHECKING:
     from ._logger import _RobocorpLogger
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 version_info = [int(x) for x in __version__.split(".")]
 
 
