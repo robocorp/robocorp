dc1053cc06b58d48374333de9e86b222d43204dd
Antero Vaissi <antero@robocorp.com>
2023-04-24 17:36:57 +0300

    Modify producer-consumer test task to use inputs/outputs

--- libs/tests/producer_consumer/task_produce.py
+++ libs/tests/producer_consumer/task_produce.py
@@ -1,7 +1,8 @@
 from pprint import PrettyPrinter
 from pathlib import Path
 
-from robo.libs.robocorp import create_output_work_item, get_inputs, get_outputs, iter_input_work_items
+from robo.libs.robocorp import get_inputs
+from robo.libs.robocorp.workitems import inputs, outputs
 from robo.libs.excel import open_workbook
 
 
@@ -9,20 +10,22 @@ pp = PrettyPrinter(indent=4)
 
 
 def task_produce():
-    for item in iter_input_work_items():
-        pp.pprint(item)
-        path = Path(item.get_file("orders.xlsx"))
-        groups = (
-            open_workbook(path).worksheet(0).as_table(header=True).group_by_column("Name")
-        )
-        for customer in groups:
-            name = customer.get_cell(0, "Name")
-            address = customer.get_cell(0, "Zip")
-            orders = customer.get_column("Item", as_list=True)
-            create_output_work_item({"Name": name, "Zip": address, "Items": orders}, save=True)
+    # for item in inputs:
+    item = next(inputs)
+    pp.pprint(item)
+    path = Path(item.get_file("orders.xlsx"))
+    groups = (
+        open_workbook(path).worksheet(0).as_table(header=True).group_by_column("Name")
+    )
+    for customer in groups:
+        name = customer.get_cell(0, "Name")
+        address = customer.get_cell(0, "Zip")
+        orders = customer.get_column("Item", as_list=True)
+        outputs.create({"Name": name, "Zip": address, "Items": orders}, save=True)
+    item.done()
 
 
 if __name__ == "__main__":
     task_produce()
     pp.pprint(f"INPUTS: {get_inputs()}")
-    pp.pprint(f"OUTPUTS: {get_outputs()}")
+    pp.pprint(f"OUTPUTS: {list(outputs)}")
