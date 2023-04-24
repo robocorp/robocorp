eb483807a30975c5b2cb6895db12df1089802812
Antero Vaissi <antero@robocorp.com>
2023-04-24 15:34:28 +0300

    Move callable globals to workitems.py

--- libs/robocorp/src/robo/libs/robocorp/__init__.py
+++ libs/robocorp/src/robo/libs/robocorp/__init__.py
@@ -1,5 +1,7 @@
 from robo.libs.robocorp.adapter import EmptyQueue, FileAdapter, RobocorpAdapter
-from robo.libs.robocorp.callables import (
+from robo.libs.robocorp.vault import Vault
+from robo.libs.robocorp.workitem import Error, State, WorkItem
+from robo.libs.robocorp.workitems import (
     create_output_work_item,
     for_each_input_work_item,
     get_inputs,
@@ -8,5 +10,3 @@ from robo.libs.robocorp.callables import (
     iter_input_work_items,
     release_input_work_item,
 )
-from robo.libs.robocorp.vault import Vault
-from robo.libs.robocorp.workitem import Error, State, WorkItem
