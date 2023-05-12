1aaba68ea2ca22406fdaf9a094c3f4f843b02cc0
Ossi Rajuvaara <ossi@robocorp.com>
2023-05-12 13:17:16 +0300

    Update templates to use new tasks/browser

--- templates/browser/tasks.py
+++ templates/browser/tasks.py
@@ -1,40 +1,35 @@
 from pathlib import Path
 from time import sleep
 
+from robocorp import browser, excel, http
 from robocorp.tasks import task
-from robocorp.http import download
-from robocorp.browser import open_url
-from robocorp.excel import open_workbook
-
-
-def read_people_from_excel():
-    return open_workbook("challenge.xlsx").worksheet("Sheet1").as_table(header=True)
-
-
-def fill_and_submit_form(page, person):
-    page.fill('//input[@ng-reflect-name="labelFirstName"]', person["First Name"])
-    page.fill('//input[@ng-reflect-name="labelLastName"]', person["Last Name"])
-    page.fill('//input[@ng-reflect-name="labelCompanyName"]', person["Company Name"])
-    page.fill('//input[@ng-reflect-name="labelRole"]', person["Role in Company"])
-    page.fill('//input[@ng-reflect-name="labelAddress"]', person["Address"])
-    page.fill('//input[@ng-reflect-name="labelEmail"]', person["Email"])
-    page.fill('//input[@ng-reflect-name="labelPhone"]', str(person["Phone Number"]))
-    page.click("input:text('Submit')")
-    sleep(0.5)
 
 
 @task
 def solve_challenge():
     """Solve the RPA challenge"""
-    page = open_url("http://rpachallenge.com/", headless=True)
+    page = browser.open_url("http://rpachallenge.com/", headless=False)
 
-    download("http://rpachallenge.com/assets/downloadFiles/challenge.xlsx")
-    people = read_people_from_excel()
+    http.download("http://rpachallenge.com/assets/downloadFiles/challenge.xlsx")
+    rows = excel.open_workbook("challenge.xlsx").worksheet("Sheet1").as_table(header=True)
 
     page.click("button:text('Start')")
-    for person in people:
-        fill_and_submit_form(page, person)
+    for row in rows:
+        fill_and_submit_form(row)
 
-    element = page.query_selector("css=div.congratulations")
+    element = page.locator("css=div.congratulations")
     element.screenshot(path=Path("output", "screenshot.png"))
     sleep(2)
+
+
+def fill_and_submit_form(row):
+    page = browser.page()
+    page.fill('//input[@ng-reflect-name="labelFirstName"]', row["First Name"])
+    page.fill('//input[@ng-reflect-name="labelLastName"]', row["Last Name"])
+    page.fill('//input[@ng-reflect-name="labelCompanyName"]', row["Company Name"])
+    page.fill('//input[@ng-reflect-name="labelRole"]', row["Role in Company"])
+    page.fill('//input[@ng-reflect-name="labelAddress"]', row["Address"])
+    page.fill('//input[@ng-reflect-name="labelEmail"]', row["Email"])
+    page.fill('//input[@ng-reflect-name="labelPhone"]', str(row["Phone Number"]))
+    page.click("input:text('Submit')")
+    sleep(0.5)
