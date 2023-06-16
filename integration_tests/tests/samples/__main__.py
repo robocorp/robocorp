d7c168a21eb9c93c5f28eed2fe648d8a1eff81d9
Fabio Zadrozny <fabiofz@gmail.com>
2023-06-16 17:17:48 -0300

    Add some samples to the integration_tests.

---
+++ integration_tests/tests/samples/__main__.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    from robocorp.tasks.cli import main
+
+    # main(("run", "-t=check_case_for"))
+    # main(("run", "-t=check_case_suppress"))
+    # main(("run", "-t=check_exception"))
+    main(("run",))
