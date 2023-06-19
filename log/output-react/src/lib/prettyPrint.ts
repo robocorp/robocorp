d2e435e95421796f731f72c6fcddd0bf3b915eda
Fabio Zadrozny <fabiofz@gmail.com>
2023-06-19 14:27:09 -0300

    log.html: improve pretty-printing of variables a bit.

--- log/output-react/src/lib/prettyPrint.ts
+++ log/output-react/src/lib/prettyPrint.ts
@@ -1,28 +1,5 @@
-function findNextSingle(i: number, curr: string, full: string): number {
-  let ignoreNext = false;
-
-  const len = full.length;
-  while (i < len) {
-    const c = full[i];
-
-    if (!ignoreNext && c === curr) {
-      return i;
-    }
-
-    if (!ignoreNext) {
-      if (c === '\\') {
-        // escaped quote, ignore the next char even if it is a ' or "
-        ignoreNext = true;
-      }
-    } else {
-      ignoreNext = false;
-    }
-
-    i++;
-  }
-
-  return i;
-}
+import { logError } from './helpers';
+import { ParsingUtils } from './parsingUtils';
 
 function formatStr(s: string): string {
   const newS = s.replaceAll('\\n', '\n');
@@ -37,56 +14,131 @@ function formatStr(s: string): string {
   }
 }
 
-/**
- * Formats a string value from the logging and shows it prettier
- * (adding new lines/indentation as needed)
- */
+export const OPTS = {
+  SMALL_LINE: 40,
+};
+
+class PrettyPrinter {
+  indentationLevel: number;
+  indentationString: string;
+  formatted: string[];
+  parsing: ParsingUtils;
+  len: number;
+  skipSpaces: boolean;
+  full: string;
+
+  constructor(full: string) {
+    this.indentationLevel = 0;
+    this.indentationString = '';
+    this.formatted = [];
+    this.full = full;
+
+    this.updateIndentationString();
+
+    this.parsing = new ParsingUtils(full, true);
+
+    this.len = full.length;
+    this.skipSpaces = false;
+  }
+
+  updateIndentationString() {
+    this.indentationString = '    '.repeat(this.indentationLevel);
+  }
+
+  formatInSingleLine(start: number, end: number) {
+    for (let i = start; i < end; i++) {
+      const currentChar = this.parsing.charAt(i);
+
+      if (currentChar === '"' || currentChar === "'") {
+        let j = this.parsing.findNextSingle(i + 1, currentChar);
+        if (j == -1) {
+          j = this.len;
+        }
+        this.formatted.push(formatStr(this.full.substring(i, j + 1)));
+        i = j;
+        continue;
+      }
+
+      if (this.skipSpaces && (currentChar == ' ' || currentChar == '\t')) {
+        continue;
+      }
+      this.skipSpaces = false;
+
+      if (currentChar == ',') {
+        this.formatted.push(currentChar, ' ');
+        this.skipSpaces = true;
+        continue;
+      }
+      this.formatted.push(currentChar);
+    }
+  }
+
+  formatStr() {
+    for (let i = 0; i < this.len; i++) {
+      const currentChar = this.parsing.charAt(i);
+
+      if (currentChar === '"' || currentChar === "'") {
+        let j = this.parsing.findNextSingle(i + 1, currentChar);
+        if (j == -1) {
+          j = this.len;
+        }
+        this.formatted.push(formatStr(this.full.substring(i, j + 1)));
+        i = j;
+        continue;
+      }
+
+      if (this.skipSpaces && (currentChar == ' ' || currentChar == '\t')) {
+        continue;
+      }
+      this.skipSpaces = false;
+
+      if (currentChar === '{' || currentChar === '[' || currentChar === '(') {
+        const j = this.parsing.eatPar(i, null, currentChar);
+        if (j == -1) {
+          this.formatted.push(currentChar);
+          continue;
+        }
+
+        if (j - i < OPTS.SMALL_LINE) {
+          this.formatInSingleLine(i, j + 1);
+          i = j;
+        } else {
+          // Add indentation after opening a bracket.
+          this.indentationLevel++;
+          this.updateIndentationString();
+          this.formatted.push(currentChar, '\n', this.indentationString);
+        }
+      } else if (currentChar === '}' || currentChar === ']' || currentChar === ')') {
+        // Decrease the indentation level after closing a bracket.
+        if (this.indentationLevel > 0) {
+          this.indentationLevel--;
+          this.updateIndentationString();
+          this.formatted.push('\n', this.indentationString, currentChar);
+        } else {
+          this.formatted.push(currentChar);
+        }
+      } else if (currentChar === ',') {
+        // Add a new line after comma
+        this.formatted.push(currentChar, '\n', this.indentationString);
+        this.skipSpaces = true;
+      } else {
+        this.formatted.push(currentChar);
+      }
+    }
+    return this.formatted.join('');
+  }
+}
+
 export function prettyPrint(full: string) {
-  let indentationLevel: number = 0;
-  let indentationString: string = '';
-  const formatted: string[] = [];
-
-  function updateIndentationString() {
-    indentationString = '    '.repeat(indentationLevel);
+  try {
+    const printer = new PrettyPrinter(full);
+    const ret = printer.formatStr();
+    // console.log(ret);
+    return ret;
+  } catch (err) {
+    // Something bad happened: show original.
+    console.log('Error pretty-printing: ' + full);
+    logError(err);
+    return full;
   }
-
-  const len = full.length;
-  let skipSpaces = false;
-  for (let i = 0; i < len; i++) {
-    const currentChar = full[i];
-
-    if (currentChar === '"' || currentChar === "'") {
-      const j = findNextSingle(i + 1, currentChar, full);
-      formatted.push(formatStr(full.substring(i, j + 1)));
-      i = j;
-      continue;
-    }
-
-    if (skipSpaces && (currentChar == ' ' || currentChar == '\t')) {
-      continue;
-    }
-    skipSpaces = false;
-
-    if (currentChar === '{' || currentChar === '[' || currentChar === '(') {
-      // Add indentation after opening a bracket.
-      indentationLevel++;
-      updateIndentationString();
-      formatted.push(currentChar, '\n', indentationString);
-    } else if (currentChar === '}' || currentChar === ']' || currentChar === ')') {
-      // Decrease the indentation level after closing a bracket.
-      indentationLevel--;
-      updateIndentationString();
-      formatted.push('\n', indentationString, currentChar);
-    } else if (currentChar === ',') {
-      // Add a new line after comma
-      formatted.push(currentChar, '\n', indentationString);
-      skipSpaces = true;
-    } else {
-      formatted.push(currentChar);
-    }
-  }
-
-  const ret = formatted.join('');
-  // console.log(ret);
-  return ret;
 }
