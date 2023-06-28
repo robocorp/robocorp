d4fabe9e3b1d8b5a57a280e0688889d52d83cccd
Ossi Rajuvaara <ossi@robocorp.com>
2023-06-28 21:04:23 +0300

    (wip) storage: fallback behaviour for missing token/endpoint

--- storage/src/robocorp/storage/_storage.py
+++ storage/src/robocorp/storage/_storage.py
@@ -1,10 +1,11 @@
 import logging
+import os
 import sys
+import urllib.parse as urlparse
 from functools import lru_cache
 from typing import Dict, Optional
 
 from ._requests import Requests, RequestsHTTPError
-from ._utils import RequiresEnv, url_join
 
 if sys.version_info >= (3, 8):
     from typing import TypedDict
@@ -41,49 +42,80 @@ class AssetUploadFailed(RuntimeError):
     """There was an unexpected error while uploading an asset."""
 
 
+def _url_join(*parts: str) -> str:
+    """Join parts into a URL and handle missing/duplicate slashes."""
+    url = ""
+    for part in parts:
+        url = urlparse.urljoin(url, part.strip("/") + "/")
+    return url
+
+
+def _get_endpoint() -> str:
+    if endpoint := os.getenv("RC_API_URL_V1"):
+        return endpoint
+
+    # Note (2023-06-28):
+    # While Control Room does set RC_API_URL_V1, this is not currently supported
+    # in local development. We can use as fallback the Vault endpoint, but we
+    # need to replace some parts to get the correct value. Examples:
+    #   api.eu1.robocloud.eu -> api.eu1.robocorp.com/v1
+    #   api.ci.robocloud.dev -> api.ci.robocorp.dev/v1
+    #   api.eu2.robocorp.com -> api.eu2.robocorp.com/v1
+    #   api.us1.robocorp.com -> api.us1.robocorp.com/v1
+    LOGGER.info("Missing environment variable 'RC_API_URL_V1', attempting fallback")
+    endpoint = os.getenv("RC_API_SECRET_HOST")
+    if endpoint is None:
+        raise RuntimeError("Missing environment variable 'RC_API_URL_V1'")
+
+    if "robocloud.eu" in endpoint:
+        endpoint = endpoint.replace("robocloud.eu", "robocorp.com")
+    elif "robocloud.dev" in endpoint:
+        endpoint = endpoint.replace("robocloud.dev", "robocorp.dev")
+
+    if not endpoint.endswith("v1"):
+        endpoint = _url_join(endpoint, "v1")
+
+    return endpoint
+
+
+def _get_token() -> str:
+    if token := os.getenv("RC_API_TOKEN_V1"):
+        return f"Bearer {token}"
+
+    # Should we support this?
+    if token := os.getenv("RC_API_KEY"):
+        return f"RC-WSKEY {token}"
+
+    # Note (2023-06-28):
+    # While Control Room does set RC_API_TOKEN_V1, this is not currently
+    # supported in local development. We can use as fallback the Vault token
+    # which is identical.
+    LOGGER.info("Missing environment variable 'RC_API_TOKEN_V1', attempting fallback")
+    if token := os.getenv("RC_API_SECRET_TOKEN"):
+        return f"Bearer {token}"
+
+    raise RuntimeError("Missing environment variable 'RC_API_TOKEN_V1'")
+
+
 @lru_cache(maxsize=1)
 def get_assets_client():
-    """Creates and returns an API client based on the injected env vars in CR."""
-    
-    # CR is using new env. variables that are not known by RCC so not set to the env.
-    # We need RC_API_URL_V1 and RC_API_KEY to be set
-    #
-    # ...or rather than using these we need to compile the values from 
-    # RC_API_SECRET_HOST and RC_API_SECRET_TOKEN values which are available
+    """
+    Creates and returns an Asset Storage API client based on the injected
+    environemnt variables from Control Room (or RCC).
+    """
+    try:
+        workspace_id = os.environ["RC_WORKSPACE_ID"]
+    except KeyError as err:
+        raise RuntimeError("Missing environment variable 'RC_WORKSPACE_ID'") from err
 
-    # RC_API_KEY we can just replace with RC_API_SECRET_TOKEN
-    # - This is the same token
+    api_endpoint = _get_endpoint()
+    api_token = _get_token()
 
-    # For RC_API_URL_V1 we need to do some tricks to get the correct url
-    # We have RC_API_SECRET_HOST in the environment, but that needs some substring replacements:
-    
-    # OLD                  -> NEW
-    # api.eu1.robocloud.eu -> api.eu1.robocorp.com/v1
-    # api.ci.robocloud.dev -> api.ci.robocorp.dev/v1
-    # api.eu2.robocorp.com -> api.eu2.robocorp.com/v1
-    # api.us1.robocorp.com -> api.us1.robocorp.com/v1
-
-    # so something like...
-    # `robocloud.eu > robocorp.com`
-    # `robocloud.dev > robocorp.dev`
-    # append `/v1` if not at the end already <- this covers eu2.robocorp.com and us1.robocorp.com
-
-
-    requires_env = RequiresEnv(
-        "Asset Storage feature can be used with Control Room only"
-    )
-    api_url = requires_env("RC_API_URL_V1")
-    workspace_id = requires_env("RC_WORKSPACE_ID")
-    api_token = requires_env("RC_API_TOKEN_V1", "")  # production availability
-    api_key = requires_env("RC_API_KEY", "")  # for local tests only
-
-    if not any([api_token, api_key]):
-        raise RuntimeError("API token or key missing")
-    auth_header = f"Bearer {api_token}" if api_token else f"RC-WSKEY {api_key}"
     default_headers = {
-        "Authorization": auth_header,
+        "Authorization": api_token,
         "Content-Type": "application/json",
     }
-    route_prefix = url_join(api_url, "workspaces", workspace_id, "assets")
+
+    route_prefix = _url_join(api_endpoint, "workspaces", workspace_id, "assets")
     assets_client = Requests(route_prefix, default_headers=default_headers)
     return assets_client
