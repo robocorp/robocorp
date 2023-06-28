acfe9afaf82dda1c318e7fe3ee2b45481b4fbfa0
Fabio Zadrozny <fabiofz@gmail.com>
2023-06-28 11:20:42 -0300

    robocorp-browser release 1.0.2

--- browser/src/robocorp/browser/__init__.py
+++ browser/src/robocorp/browser/__init__.py
@@ -11,7 +11,7 @@ from playwright.sync_api import (
 
 from ._browser_engines import BrowserEngine
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 version_info = [int(x) for x in __version__.split(".")]
 
 
