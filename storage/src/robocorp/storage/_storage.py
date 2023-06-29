6e6ee90bbd94ae4675055c161788826eba73b1a3
Ossi R <ossi@robocorp.com>
2023-06-29 14:40:39 +0300

    Storage cloud tokens workaround (#37)
    * Pseudo
    
    * ..pseudo updated
    
    * (wip) storage: fallback behaviour for missing token/endpoint
    
    * Remove API key support, add more robust endpoint conversion
    
    * Do not replace from middle of url
    
    * Add some tests
    
    * storage: bump version
    
    ---------
    
    Co-authored-by: Kari Harju <kari@robocorp.com>

--- storage/src/robocorp/storage/_storage.py
+++ storage/src/robocorp/storage/_storage.py
@@ -1,10 +1,11 @@
 import logging
+import os
 import sys
 from functools import lru_cache
 from typing import Dict, Optional
+from urllib.parse import urljoin, urlsplit, urlunsplit
 
 from ._requests import Requests, RequestsHTTPError
-from ._utils import RequiresEnv, url_join
 
 if sys.version_info >= (3, 8):
     from typing import TypedDict
@@ -41,24 +42,84 @@ class AssetUploadFailed(RuntimeError):
     """There was an unexpected error while uploading an asset."""
 
 
+def _url_join(*parts: str) -> str:
+    url = ""
+    for part in parts:
+        url = urljoin(url, part.strip("/") + "/")
+    return url
+
+
+def _get_endpoint() -> str:
+    if endpoint := os.getenv("RC_API_URL_V1"):
+        return endpoint
+
+    # Note (2023-06-28):
+    # While Control Room does set RC_API_URL_V1, this is not currently supported
+    # in local development. We can use as fallback the Vault endpoint, but we
+    # need to replace some parts to get the correct value. Examples:
+    #   api.eu1.robocloud.eu -> api.eu1.robocorp.com/v1
+    #   api.ci.robocloud.dev -> api.ci.robocorp.dev/v1
+    #   api.eu2.robocorp.com -> api.eu2.robocorp.com/v1
+    #   api.us1.robocorp.com -> api.us1.robocorp.com/v1
+    LOGGER.info("Missing environment variable 'RC_API_URL_V1', attempting fallback")
+    endpoint = os.getenv("RC_API_SECRET_HOST")
+    if endpoint is None:
+        raise RuntimeError("Missing environment variable 'RC_API_URL_V1'")
+
+    url = urlsplit(endpoint)
+
+    # Replace hostname to match correct API
+    netloc = url.netloc
+    if url.hostname and url.hostname.endswith("robocloud.eu"):
+        netloc = netloc.replace("robocloud.eu", "robocorp.com")
+    elif url.hostname and url.hostname.endswith("robocloud.dev"):
+        netloc = netloc.replace("robocloud.dev", "robocorp.dev")
+    url = url._replace(netloc=netloc)
+
+    # Append /v1/ to path if not already there
+    if not url.path.rstrip("/").endswith("v1"):
+        path = _url_join(url.path, "v1")
+        url = url._replace(path=path)
+
+    result = urlunsplit(url)
+    LOGGER.info("Adapted endpoint: %s -> %s", endpoint, result)
+    return result
+
+
+def _get_token() -> str:
+    if token := os.getenv("RC_API_TOKEN_V1"):
+        return token
+
+    # Note (2023-06-28):
+    # While Control Room does set RC_API_TOKEN_V1, this is not currently
+    # supported in local development. We can use as fallback the Vault token
+    # which is identical.
+    LOGGER.info("Missing environment variable 'RC_API_TOKEN_V1', attempting fallback")
+    if token := os.getenv("RC_API_SECRET_TOKEN"):
+        return token
+
+    raise RuntimeError("Missing environment variable 'RC_API_TOKEN_V1'")
+
+
 @lru_cache(maxsize=1)
 def get_assets_client():
-    """Creates and returns an API client based on the injected env vars in CR."""
-    requires_env = RequiresEnv(
-        "Asset Storage feature can be used with Control Room only"
-    )
-    api_url = requires_env("RC_API_URL_V1")
-    workspace_id = requires_env("RC_WORKSPACE_ID")
-    api_token = requires_env("RC_API_TOKEN_V1", "")  # production availability
-    api_key = requires_env("RC_API_KEY", "")  # for local tests only
+    """
+    Creates and returns an Asset Storage API client based on the injected
+    environment variables from Control Room (or RCC).
+    """
+    try:
+        workspace_id = os.environ["RC_WORKSPACE_ID"]
+    except KeyError as err:
+        raise RuntimeError("Missing environment variable 'RC_WORKSPACE_ID'") from err
 
-    if not any([api_token, api_key]):
-        raise RuntimeError("API token or key missing")
-    auth_header = f"Bearer {api_token}" if api_token else f"RC-WSKEY {api_key}"
+    api_token = _get_token()
+    api_endpoint = _get_endpoint()
+
     default_headers = {
-        "Authorization": auth_header,
+        "Authorization": f"Bearer {api_token}",
         "Content-Type": "application/json",
     }
-    route_prefix = url_join(api_url, "workspaces", workspace_id, "assets")
+
+    route_prefix = _url_join(api_endpoint, "workspaces", workspace_id, "assets")
     assets_client = Requests(route_prefix, default_headers=default_headers)
     return assets_client
---
+++ storage/tests/test_fallback.py
@@ -0,0 +1,71 @@
+"""
+Unittests for testing fallback behaviour for missing Asset Storage
+endpoint and token in local runs.
+"""
+import os
+from unittest.mock import patch
+
+import pytest
+
+from robocorp.storage._storage import _get_endpoint, _get_token
+
+ENDPOINTS = [
+    ("https://api.eu1.robocloud.eu/", "https://api.eu1.robocorp.com/v1/"),
+    ("https://api.ci.robocloud.dev/", "https://api.ci.robocorp.dev/v1/"),
+    ("https://api.eu2.robocorp.com/", "https://api.eu2.robocorp.com/v1/"),
+    ("https://api.us1.robocorp.com/", "https://api.us1.robocorp.com/v1/"),
+    ("https://api.us1.robocorp.com/v1", "https://api.us1.robocorp.com/v1"),
+    ("https://api.us1.robocorp.com/v1/", "https://api.us1.robocorp.com/v1/"),
+    ("api.us1.robocorp.com", "api.us1.robocorp.com/v1/"),
+    ("https://api.eu1.robocorp.com/v1/", "https://api.eu1.robocorp.com/v1/"),
+    (
+        "https://api.robocloud.dev.enterprise.com/v1/",
+        "https://api.robocloud.dev.enterprise.com/v1/",
+    ),
+    ("https://api.eu1.robocloud.eu:8080/", "https://api.eu1.robocorp.com:8080/v1/"),
+]
+
+
+def test_endpoint_expected():
+    with patch.dict(
+        os.environ,
+        {"RC_API_URL_V1": "correct", "RC_API_SECRET_HOST": "wrong"},
+        clear=True,
+    ):
+        result = _get_endpoint()
+        assert result == "correct"
+
+
+@pytest.mark.parametrize("value,expected", ENDPOINTS)
+def test_endpoint_fallback(value, expected):
+    with patch.dict(os.environ, {"RC_API_SECRET_HOST": value}, clear=True):
+        result = _get_endpoint()
+        assert result == expected
+
+
+def test_endpoint_none():
+    with patch.dict(os.environ, {}, clear=True):
+        with pytest.raises(RuntimeError):
+            _get_endpoint()
+
+
+def test_token_expected():
+    with patch.dict(
+        os.environ,
+        {"RC_API_TOKEN_V1": "correct", "RC_API_SECRET_TOKEN": "wrong"},
+        clear=True,
+    ):
+        result = _get_token()
+        assert result == "correct"
+
+
+def test_token_fallback():
+    with patch.dict(os.environ, {"RC_API_SECRET_TOKEN": "correct"}, clear=True):
+        result = _get_token()
+        assert result == "correct"
+
+
+def test_token_none():
+    with patch.dict(os.environ, {}, clear=True):
+        with pytest.raises(RuntimeError):
+            _get_token()
