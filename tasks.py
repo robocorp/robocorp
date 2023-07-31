af0132cbf83f195ca23bae0edef5d227c46a444c
Ossi R <ossi@robocorp.com>
2023-07-31 10:46:56 +0300

    robcorp-workitems release 1.3.0

--- tasks.py
+++ tasks.py
@@ -7,17 +7,10 @@ from invoke import task
 CURDIR: Path = Path(__file__).absolute().parent
 
 
-def iter_project_dirs():
-    for d in CURDIR.iterdir():
-        if d.is_dir():
-            if (d / "pyproject.toml").exists():
-                yield d
-
-
 @task
 def poetry_lock(ctx: invoke.Context) -> None:
     """Runs 'poetry lock' on all projects with a pyproject.toml."""
-    for project_dir in iter_project_dirs():
+    for project_dir in _iter_project_dirs():
         # Note: can't use threads to speed it up. poetry (or pip) just fail...
         subprocess.check_call(["poetry", "lock"], cwd=project_dir)
 
@@ -31,9 +24,61 @@ def docs(ctx: invoke.Context) -> None:
         "meta",
     ]
 
-    for project_dir in iter_project_dirs():
+    for project_dir in _iter_project_dirs():
         if project_dir.name in ignored:
             continue
 
         subprocess.check_call(["poetry", "install"], cwd=project_dir)
         subprocess.check_call(["poetry", "run", "invoke", "docs"], cwd=project_dir)
+
+
+@task
+def unreleased(ctx: invoke.Context) -> None:
+    """Compare current branch to PyPI."""
+    import tomlkit
+
+    ignored = [
+        "devutils",
+        "integration_tests",
+    ]
+
+    ok = True
+    for project_dir in _iter_project_dirs():
+        if project_dir.name in ignored:
+            continue
+
+        pyproject = project_dir / "pyproject.toml"
+        contents = tomlkit.loads(pyproject.read_text())
+        poetry = contents["tool"]["poetry"]
+
+        name = poetry["name"]
+        current = poetry["version"]
+        remote = _pypi_version(name)
+
+        if current != remote:
+            print(f"Package '{name}' version mismatch: {current} != {remote}")
+            ok = False
+
+    if ok:
+        print("All packages up-to-date")
+    else:
+        raise invoke.Exit(code=1)
+
+
+def _iter_project_dirs():
+    for path in CURDIR.iterdir():
+        if path.is_dir():
+            if (path / "pyproject.toml").exists():
+                yield path
+
+
+def _pypi_version(name: str) -> str:
+    import json
+    import urllib.request
+    import semver
+
+    with urllib.request.urlopen(f"https://pypi.org/pypi/{name}/json") as response:
+        metadata = json.loads(response.read())
+
+    latest = max(metadata["releases"].keys(), key=semver.Version.parse)
+    return latest
