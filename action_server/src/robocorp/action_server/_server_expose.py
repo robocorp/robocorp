998d49b1ce29c43216566fb55110d242a1536f17
Kristaps Taube <t.kristaps@gmail.com>
2023-12-04 17:02:01 +0200

    chore: remove indirection

--- action_server/src/robocorp/action_server/_server_expose.py
+++ action_server/src/robocorp/action_server/_server_expose.py
@@ -16,42 +16,42 @@ class BodyPayload(BaseModel):
     path: str
 
 
-async def listen_for_requests(app: FastAPI, url: str):
-    async with websockets.connect(f"wss://client.{url}") as ws:
-        while True:
-            message = await ws.recv()
-
-            data = json.loads(message)
-
-            try:
-                payload = SessionPayload(**data)
-                print(f"🌍 https://{payload.sessionId}.{url}/openapi.json")
-                continue
-            except Exception:
-                pass
-
-            try:
-                payload = BodyPayload(**data)
-
-                if payload.path == "/openapi.json":
-                    await ws.send(
-                        json.dumps(
-                            {
-                                "requestId": payload.requestId,
-                                "response": json.dumps(
-                                    app.openapi(),
-                                    indent=2,
-                                ),
-                            }
-                        )
-                    )
-                    continue
-            except Exception:
-                pass
-
-
 async def expose_server(app: FastAPI, url: str):
     """
     Exposes the server to the world.
     """
-    asyncio.create_task(listen_for_requests(app=app, url=url))
+
+    async def listen_for_requests():
+        async with websockets.connect(f"wss://client.{url}") as ws:
+            while True:
+                message = await ws.recv()
+
+                data = json.loads(message)
+
+                try:
+                    payload = SessionPayload(**data)
+                    print(f"🌍 https://{payload.sessionId}.{url}/openapi.json")
+                    continue
+                except Exception:
+                    pass
+
+                try:
+                    payload = BodyPayload(**data)
+
+                    if payload.path == "/openapi.json":
+                        await ws.send(
+                            json.dumps(
+                                {
+                                    "requestId": payload.requestId,
+                                    "response": json.dumps(
+                                        app.openapi(),
+                                        indent=2,
+                                    ),
+                                }
+                            )
+                        )
+                        continue
+                except Exception:
+                    pass
+
+    asyncio.create_task(listen_for_requests())
