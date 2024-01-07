4157efe9e5f9ee61c4dd14294cee0288ddfcb7b9
Bogdan Condurache <bogdan@robocorp.com>
2024-01-08 01:33:44 +0200

    fix full path building using  for windows

--- action_server/go-wrapper/main.go
+++ action_server/go-wrapper/main.go
@@ -24,10 +24,6 @@ func copyFiles(src, dest string) error {
 		srcPath := fmt.Sprintf("%s/%s", src, file.Name())
 		destPath := filepath.Join(dest, file.Name())
 
-		if file.Name() == ".gitkeep" {
-			continue
-		}
-
 		if file.IsDir() {
 			err := copyFiles(srcPath, destPath)
 			if err != nil {
