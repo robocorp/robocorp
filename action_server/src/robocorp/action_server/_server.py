a7ac2c2c1e0cb3b6d3fbe532b21e76d6e43f92d2
Rihards Gravis <rihards@gravis.lv>
2024-01-08 13:22:15 +0200

    Fox formatting

--- action_server/src/robocorp/action_server/_server.py
+++ action_server/src/robocorp/action_server/_server.py
@@ -122,22 +122,24 @@ def start_server(
 
     @app.get("/config", include_in_schema=False)
     async def serve_config():
-        payload = {
-            "expose_url": False
-        }
+        payload = {"expose_url": False}
 
-        if expose: 
+        if expose:
             current_expose_session = read_expose_session_json(
-                    datadir=str(settings.datadir)
-                )
-            
-            expose_session_payload = get_expose_session_payload(
-                current_expose_session.expose_session
-            ) if current_expose_session else None
+                datadir=str(settings.datadir)
+            )
+
+            expose_session_payload = (
+                get_expose_session_payload(current_expose_session.expose_session)
+                if current_expose_session
+                else None
+            )
 
             if expose_session_payload:
-                payload["expose_url"] = f"https://{expose_session_payload.sessionId}.{settings.expose_url}"
-                
+                payload[
+                    "expose_url"
+                ] = f"https://{expose_session_payload.sessionId}.{settings.expose_url}"
+
         return payload
 
     @app.get("/base_log.html", response_class=HTMLResponse)
