9e501d9857b826bcb14a8534094278f3e77f4996
Fabio Zadrozny <fabiofz@gmail.com>
2024-01-08 09:34:19 -0300

    Release robocorp-log-pytest 0.0.2

--- log_pytest/src/robocorp/log_pytest/__init__.py
+++ log_pytest/src/robocorp/log_pytest/__init__.py
@@ -14,7 +14,7 @@ from robocorp.log.redirect import setup_stdout_logging
 
 from robocorp import log
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 version_info = [int(x) for x in __version__.split(".")]
 
 
