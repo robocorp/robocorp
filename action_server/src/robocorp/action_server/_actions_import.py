b567a2f05910368076ac46eb075933a8b83da8e9
Kristaps Taube <t.kristaps@gmail.com>
2024-01-09 09:33:18 +0200

    Update action_server/src/robocorp/action_server/_actions_import.py
    Co-authored-by: Rihards Gravis <rihards@gravis.lv>

--- action_server/src/robocorp/action_server/_actions_import.py
+++ action_server/src/robocorp/action_server/_actions_import.py
@@ -120,7 +120,7 @@ Note: no virtual environment will be used for the imported actions, they'll be r
     if not name:
         name = import_path.name
 
-    log.info(f"Python Interpretor path: {use_env.get('RCC_HOLOTREE_SPACE_ROOT')}/bin/python")
+    log.info(f"Python Interpreter path: {use_env.get('RCC_HOLOTREE_SPACE_ROOT')}/bin/python")
 
     action_package = ActionPackage(
         id=action_package_id,
