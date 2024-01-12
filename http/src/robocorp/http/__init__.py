d688b2a30f37f09cb6f23dcb05b355956fd7bd4f
Cosmin Poieana <cosmin@robocorp.com>
2024-01-12 19:06:18 +0300

    Bring back a deployable project (as deprecated)

---
+++ http/src/robocorp/http/__init__.py
@@ -0,0 +1,11 @@
+import warnings
+
+__version__ = "0.4.1"
+
+warnings.warn(
+    "This library is DEPRECATED, please use `requests` instead! "
+    "(https://github.com/robocorp/robocorp/blob/master/docs/3rd_party/"
+    "requests/README.md)",
+    DeprecationWarning,
+    stacklevel=2,
+)
