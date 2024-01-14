34d2ab4dfeef50038fe91eb220fc8771bea9d724
Cosmin Poieana <cosmin@robocorp.com>
2024-01-14 09:43:12 +0300

    vault: 1.3.1

--- vault/src/robocorp/vault/__init__.py
+++ vault/src/robocorp/vault/__init__.py
@@ -5,7 +5,7 @@ from typing import Any
 from ._errors import RobocorpVaultError
 from ._secrets import SecretContainer
 
-__version__ = "1.3.0"
+__version__ = "1.3.1"
 version_info = [int(x) for x in __version__.split(".")]
 
 
