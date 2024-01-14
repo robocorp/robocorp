8c6feea990a92cc2971654714e739fb352575882
Cosmin Poieana <cosmin@robocorp.com>
2024-01-14 09:13:54 +0300

    excel: 0.4.2

--- excel/src/robocorp/excel/__init__.py
+++ excel/src/robocorp/excel/__init__.py
@@ -1,7 +1,7 @@
 from robocorp.excel.excel import create_workbook, open_workbook
 from robocorp.excel.tables import Table
 
-__version__ = "0.4.1"
+__version__ = "0.4.2"
 __all__ = [
     "create_workbook",
     "open_workbook",
