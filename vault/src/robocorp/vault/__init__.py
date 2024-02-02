756d12e22f8104c6ae1464a32a37b91f3931be59
Fabio Zadrozny <fabiofz@gmail.com>
2024-02-02 16:18:24 -0300

    Release robocorp-vault 1.3.3

--- vault/src/robocorp/vault/__init__.py
+++ vault/src/robocorp/vault/__init__.py
@@ -5,7 +5,7 @@ from typing import Any
 from ._errors import RobocorpVaultError
 from ._secrets import SecretContainer
 
-__version__ = "1.3.2"
+__version__ = "1.3.3"
 version_info = [int(x) for x in __version__.split(".")]
 
 
