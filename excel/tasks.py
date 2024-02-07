9ccca2dbaa65609427cab0540ab97c39fc1ba114
Cosmin Poieana <cosmin@robocorp.com>
2024-02-07 19:22:08 +0300

    An improved way of loading `devutils` in `excel`

--- excel/tasks.py
+++ excel/tasks.py
@@ -1,14 +1,23 @@
-import sys
+from importlib.machinery import SourceFileLoader
 from pathlib import Path
 
-# Add the devutils even if the poetry env isn't setup (to do a 'inv devinstall').
+# Since the `robocorp-devutils` dependency is available in the local Poetry isolated
+#  environment only, we won't have it available with Invoke commands running outside
+#  such environment.
+tasks_path = Path(__file__).absolute()
+invoke_utils_path = (
+    tasks_path.parent.parent /
+    "devutils" / "src" / "devutils" / "invoke_utils.py"
+)
+assert invoke_utils_path.is_file(), f"{invoke_utils_path} does not exist"
+
+# But we still need to import the package in order to expose the common Invoke tasks.
 try:
-    import devutils
+    from devutils import invoke_utils
 except ImportError:
-    devutils_src = Path(__file__).absolute().parent.parent / "devutils" / "src"
-    assert devutils_src.exists(), f"{devutils_src} does not exist."
-    sys.path.append(str(devutils_src))
+    invoke_utils = SourceFileLoader(
+        "invoke_utils", str(invoke_utils_path)
+    ).load_module()
 
-from devutils.invoke_utils import build_common_tasks
-
-globals().update(build_common_tasks(Path(__file__).absolute().parent, "robocorp.excel"))
+common_tasks = invoke_utils.build_common_tasks(tasks_path.parent, "robocorp.excel")
+globals().update(common_tasks)
