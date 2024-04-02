1d1ada2b865006f1eb54f8b47f244f9f80f19f28
Fabio Zadrozny <117621+fabioz@users.noreply.github.com>
2024-04-02 11:13:48 -0300

    action-server package build implemented.

---
+++ action_server/tests/action_server_tests/test_package.py
@@ -0,0 +1,74 @@
+import os
+import re
+import zipfile
+
+
+def check_regexp_in_lines(text, regexp):
+    """Checks if the given regexp is found in any line of the text.
+
+    Args:
+      text: The text to search in.
+      regexp: The regular expression to search for.
+
+    Returns:
+      True if the regexp is found in any line, False otherwise.
+    """
+
+    compiled_regexp = re.compile(regexp)
+    for line in text.splitlines():
+        if compiled_regexp.search(line) is not None:
+            return
+    raise RuntimeError(f"Did not find regexp: {regexp} in text:\n{text}")
+
+
+def test_package_zip(datadir):
+    from robocorp.action_server._selftest import robocorp_action_server_run
+
+    output = robocorp_action_server_run(
+        [
+            "package",
+            "build",
+            "--output-dir",
+            str(datadir),
+            "--datadir",
+            str(datadir / "data"),
+            # "-v",
+        ],
+        returncode=0,
+        cwd=datadir / "pack1",
+    )
+    zip_name = "pack1-name-0.2a1.zip"
+    escaped = re.escape(zip_name)
+    check_regexp_in_lines(output.stderr, f"Created(.*){escaped}")
+
+    target_zip = datadir / zip_name
+    assert os.path.exists(target_zip)
+
+    with zipfile.ZipFile(target_zip, "r") as zip_file:
+        file_names = set(zip_file.namelist())
+
+    assert file_names == {
+        "another/ok.txt",
+        "folder/dont_ignore",
+        "hello_action.py",
+        "package.yaml",
+        "folder/ignore_only_at_root",
+    }
+
+
+def test_package_zip_no_actions(datadir):
+    from robocorp.action_server._selftest import robocorp_action_server_run
+
+    output = robocorp_action_server_run(
+        [
+            "package",
+            "build",
+            "--output-dir",
+            str(datadir),
+            "--datadir",
+            str(datadir / "data"),
+        ],
+        returncode=1,
+        cwd=datadir / "pack2",
+    )
+    assert "No actions found in " in output.stderr
