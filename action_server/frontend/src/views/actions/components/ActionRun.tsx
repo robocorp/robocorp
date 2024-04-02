3e755a50fac19d9af03e31696b9b9936086e39a6
Cosmin G. Popa <cpopa@robocorp.com>
2024-04-02 19:07:03 +0300

    Formatting result for the code mirror editor to show returns correctly. Some small stylisation to the Run drawer (#313)

--- action_server/frontend/src/views/actions/components/ActionRun.tsx
+++ action_server/frontend/src/views/actions/components/ActionRun.tsx
@@ -1,6 +1,16 @@
 /* eslint-disable react/no-array-index-key */
 import { FC, FormEvent, ReactNode, useCallback, useEffect, useState } from 'react';
-import { Box, Button, Checkbox, Form, Input, Select, Typography } from '@robocorp/components';
+import {
+  Box,
+  Button,
+  Checkbox,
+  Divider,
+  Form,
+  Input,
+  Select,
+  Typography,
+} from '@robocorp/components';
+import { IconBolt } from '@robocorp/icons/iconic';
 
 import { Action, ActionPackage } from '~/lib/types';
 import { toKebabCase } from '~/lib/helpers';
@@ -219,12 +229,25 @@ export const ActionRun: FC<Props> = ({ action, actionPackage }) => {
           }
         })}
       </Form.Fieldset>
-      <Button.Group align="right">
-        <Button loading={isPending} type="submit" variant="primary">
-          Run
+      <Button.Group align="right" marginBottom={16}>
+        <Button
+          loading={isPending}
+          type="submit"
+          variant="primary"
+          icon={IconBolt}
+          style={{ width: '160px' }}
+        >
+          {isPending ? 'Executing...' : 'Execute Action'}
         </Button>
       </Button.Group>
-      {isSuccess && <ActionRunResult result={data.response} runId={data.runId} />}
+      <Divider />
+      {isSuccess && (
+        <ActionRunResult
+          result={data.response}
+          runId={data.runId}
+          outputSchemaType={JSON.parse(action.output_schema) as { type: string }}
+        />
+      )}
     </Form>
   );
 };
