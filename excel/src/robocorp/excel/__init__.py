b6078ad7222da40a60904943b5cdc50c3f95fb9e
Vlad <vlad@sema4.ai>
2024-09-25 11:41:50 +0300

    Bump version

--- excel/src/robocorp/excel/__init__.py
+++ excel/src/robocorp/excel/__init__.py
@@ -9,7 +9,7 @@ in your dependencies' configuration.
 from robocorp.excel.excel import create_workbook, open_workbook
 from robocorp.excel.tables import Table
 
-__version__ = "0.4.4"
+__version__ = "0.4.5"
 __all__ = [
     "create_workbook",
     "open_workbook",
