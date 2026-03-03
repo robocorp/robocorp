b700410e226f88e3f2f179c76e945126ebebe62c
raivolink <raivo@sema4.ai>
2026-03-03 13:45:34 +0200

    Chrome test adjustment

--- windows/tests/robocorp_windows_tests/test_robocorp_windows_chrome.py
+++ windows/tests/robocorp_windows_tests/test_robocorp_windows_chrome.py
@@ -27,12 +27,14 @@ def _check_multiple_interactions():
     # In GitHub Actions on a fresh install Chrome asks to sign in.
     # We have to decline
     try:
-        app = windows.find_window("regex:.*Sign in to Chrome", timeout=5)
+        app = windows.find_window("regex:.*Google Chrome", timeout=5)
         app.find('control:"ButtonControl" and name:"Close"').click()
     except windows.ElementNotFound:
         pass  # Ignore if not there.
 
-    w = windows.find_window("regex:.*Google Chrome", wait_time=0.5, timeout=5)
+    w = windows.find_window(
+        "regex:New Tab.*Google Chrome", wait_time=0.5, timeout=5
+    )
     w.send_keys("{Alt}d", wait_time=0.2, send_enter=False)
     w.send_keys(url, wait_time=3, send_enter=True)
 
@@ -49,7 +51,9 @@ def _check_multiple_interactions():
 
     # Test drag and drop
     div_header_item = w.find("id:mydiv > id:mydivheader", search_depth=12)
-    move_item = w.find("id:mydiv > control:TextControl name:Move", search_depth=12)
+    move_item = w.find(
+        "id:mydiv > control:TextControl name:Move", search_depth=12
+    )
 
     original_top = div_header_item.top
     desktop.drag_and_drop(div_header_item, move_item)
