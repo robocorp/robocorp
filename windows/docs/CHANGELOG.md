183257b438bc6ea1a1a0a3df4ac268a5dc47a4f3
raivolink <raivo@sema4.ai>
2026-03-13 12:44:41 +0200

    Update psutil in tasks and windows to match other packages

--- windows/docs/CHANGELOG.md
+++ windows/docs/CHANGELOG.md
@@ -2,6 +2,10 @@
 
 ## Unreleased
 
+## 1.1.1 - 2026-03-13
+
+- Update `psutil` to match other packages
+
 ## 1.1.0 - 2026-03-12
 
 - Dependecy updates
--- windows/src/robocorp/windows/__init__.py
+++ windows/src/robocorp/windows/__init__.py
@@ -29,7 +29,7 @@ from .protocols import Locator
 if typing.TYPE_CHECKING:
     from PIL.Image import Image
 
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 version_info = [int(x) for x in __version__.split(".")]
 
 
